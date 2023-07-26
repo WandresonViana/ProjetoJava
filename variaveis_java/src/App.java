037769e47c1c7c46894be46fbfcd49841dfcd02a
Wandreson Viana <wandresonviana01@gmail.com>
2023-07-26 20:41:44 -0300

    Adicionando variáveis

---
+++ variaveis_java/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Tipos de variáveis
+        String mensagem = "Mensagem de texto";
+        int inteiro = 5;
+        float pontoFlutuante = 5.4f;
+        boolean condicao = true;
+
+        //Mostrando no terminal
+        System.out.println("Esta é uma mensagem em String: " + mensagem);
+        System.out.println("Este é um número inteiro: " + inteiro);
+        System.out.println("Este é um númrero com ponto flutuante: " + pontoFlutuante);
+        System.out.println("Esta é uma condicinal lógica: " + condicao);
+    }
+}
