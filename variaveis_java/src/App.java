f15cca2eefa10271de6525476c9abd2b885ff194
Wandreson Viana <wandresonviana01@gmail.com>
2023-07-27 20:53:28 -0300

    Trabalhando com entrada de dados

--- variaveis_java/src/App.java
+++ variaveis_java/src/App.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        //objeto para entrada de dados
+        Scanner entrada = new Scanner(System.in);
+
         //Tipos de variáveis
         String mensagem = "Mensagem de texto";
         int inteiro = 5;
         float pontoFlutuante = 5.4f;
         boolean condicao = true;
+
+        //Trabalhando com entrada de dados
+        System.out.println("Digite uma mensagem com caracteres: ");
+        mensagem = entrada.nextLine();
+        System.out.println("Digite um número inteiro: ");
+        inteiro = entrada.nextInt();
+        System.out.println("Digite um número com ponto flutuante: ");
+        pontoFlutuante = entrada.nextFloat();
+
 
         //Mostrando no terminal
         System.out.println("Esta é uma mensagem em String: " + mensagem);
