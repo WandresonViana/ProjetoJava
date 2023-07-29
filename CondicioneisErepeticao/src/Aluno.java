fa0ec4e253be9c72f6b519c3c46b66beddf86645
Wandreson Viana <wandresonviana01@gmail.com>
2023-07-29 18:14:37 -0300

    iniciando o IF

---
+++ CondicioneisErepeticao/src/Aluno.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Aluno {
+    public static void main(String[] args) throws Exception {
+        //Objeto para entrada de dados
+        Scanner entrada = new Scanner(System.in);
+
+
+        //Usando média do aluno como exemplo
+        String aluno;
+        float nota1;
+        float nota2;
+        float media;
+        boolean statusdoAluno;
+
+
+        System.out.print("Digite o nome do aluno(a): ");
+        aluno = entrada.nextLine();
+        System.out.print("Digite a primeira nota do aluno: ");
+        nota1 = entrada.nextFloat();
+        System.out.println("Digite a segunda nota do aluno: ");
+        nota2 = entrada.nextFloat();
+
+        media = (nota1 + nota2)/ 2;
+
+        if(media >= 6){
+            statusdoAluno = true;
+        }else{
+            statusdoAluno = false;
+        }
+
+
+        System.out.printf("%s teve como primeira nota: %.2f e como segunda nota: %.2f",aluno,nota1,nota2);
+
+
+    }
+}
