9a389cbf95001ade9d9d461fb801be5e5e5deb9a
Wandreson Viana <wandresonviana01@gmail.com>
2023-07-29 17:58:36 -0300

    Iniciando aluno

--- CondicioneisErepeticao/src/App.java
+++ CondicioneisErepeticao/src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Objeto para entrada de dados
+        Scanner entrada = new Scanner(System.in);
+
+
+        //Usando média do aluno como exemplo
+        String nomeAluno;
+        float nota1;
+        float nota2;
+        float media;
+
+
     }
 }
