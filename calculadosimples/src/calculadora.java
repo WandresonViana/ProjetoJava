a753951521668f9cf3e384b2e3424b3a73abeda7
Wandreson Viana <wandresonviana01@gmail.com>
2023-07-31 20:22:24 -0300

    Update calculadora.java

--- calculadosimples/src/calculadora.java
+++ calculadosimples/src/calculadora.java
@@ -5,7 +5,7 @@ public class calculadora {
         Scanner entrada = new Scanner(System.in);
         float primeiroValor;
         float segundoValor;
-        float resultado;
+        float resultado = 0;
         String operador;
         String saida = " ";
 
@@ -15,15 +15,35 @@ public class calculadora {
             primeiroValor = entrada.nextFloat();
             System.out.print("Digite o segundo valor: ");
             segundoValor = entrada.nextFloat();
+            System.out.println(" ");
             System.out.println("Digite [+] Para somar");
             System.out.println("Digite [-] para subtrair");
             System.out.println("Digite [*] para multiplicar");
             System.out.println("Digite [/] para dividir");
             operador = entrada.nextLine();
-            
 
+            switch(operador){
+                case "+":
+                    resultado = primeiroValor + segundoValor;
+                    break;
+                case "-":
+                    resultado = primeiroValor - segundoValor;
+                    break;
+                case "*":
+                    resultado = primeiroValor * segundoValor;
+                    break;
+                case "/":
+                    resultado = primeiroValor / segundoValor;
+                    break;
+                default:
+                    System.out.println("Erro!");
+                    break;
+            }
+            System.out.println(" ");
+            System.out.println("O resultado é: " + resultado);
 
             //saida do programa
+
             System.out.print("Digite [S] para sair: ");
             saida = entrada.nextLine();
             if(saida.equals("S")){
