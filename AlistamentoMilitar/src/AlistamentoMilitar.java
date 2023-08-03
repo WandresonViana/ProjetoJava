aac9bbbc40e8a7397580a81a3841d73e30f6ad6a
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-02 21:17:44 -0300

    Update AlistamentoMilitar.java

--- AlistamentoMilitar/src/AlistamentoMilitar.java
+++ AlistamentoMilitar/src/AlistamentoMilitar.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class AlistamentoMilitar {
     public static void main(String[] args) throws Exception {
-     String nomePessoa;
-     int idade;
+        Scanner leitura = new Scanner(System.in);
+        String nomePessoa;
+        int idade;
+
+        System.out.print("Digite o Seu nome: ");
+        nomePessoa = leitura.nextLine();
+        System.out.print("Digite a sua Idade: ");
+        idade = leitura.nextInt();
+
+        if(idade < 16){
+            
+        }
+
      
     }
 }
