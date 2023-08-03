4e81c36f65f26ba0f9a4c965bc74a4039d0d44fe
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-03 20:50:26 -0300

    Outra forma de iniciar vetor

--- Vetores/src/Vetores.java
+++ Vetores/src/Vetores.java
@@ -1,6 +1,12 @@
 public class Vetores {
     public static void main(String[] args) throws Exception {
         int n[] = new int[4];
+        n[0] = 3;
+        n[1] = 4;
+        n[2] = 8;
+        n[3] = 6;
+
+        int m[] = {4,8,6,2};
         
     }
 }
