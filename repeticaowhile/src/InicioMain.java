8b2984fe4d388ae6c7b9a40e9e39a2ba3e60568c
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-26 17:54:54 -0300

    contador simples

--- repeticaowhile/src/InicioMain.java
+++ repeticaowhile/src/InicioMain.java
@@ -1,5 +1,13 @@
 public class InicioMain {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int cont = 0;
+
+        while(cont < 8){
+            System.out.println(cont);
+            cont++;
+        }
+
+
     }
 }
