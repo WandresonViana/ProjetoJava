f5a9c281eb8fa89782401cb6aabc1423ed431cba
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-27 23:27:56 -0300

    For para mostrar os valores

--- repeticaowhile/src/InicioMain.java
+++ repeticaowhile/src/InicioMain.java
@@ -4,14 +4,18 @@ public class InicioMain {
     public static void main(String[] args) throws Exception {
         Scanner en = new Scanner(System.in);
         int novo[] = new int[5];
-
+        
         int cont = 0;
+        
 
         while(cont < 5){
             System.out.println("Digite um valor");
             novo[cont] = en.nextInt();
-            System.out.println(novo[cont]);
             cont++;
+        }
+
+        for(int cot = 0; cot < novo.length; cot++){
+            System.out.print(novo[cot] + " ");
         }
         
     }
