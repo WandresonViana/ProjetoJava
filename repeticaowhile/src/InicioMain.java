6c5daba0df75d0443f02dd225f93faec59a50d65
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-28 23:55:56 -0300

    Update InicioMain.java

--- repeticaowhile/src/InicioMain.java
+++ repeticaowhile/src/InicioMain.java
@@ -8,7 +8,7 @@ public class InicioMain {
         int cont = 0;
         int maior = 0;
         int menor = 0;
-        
+
         while(cont < 5){
             System.out.println("Digite um valor");
             novo[cont] = en.nextInt();
@@ -27,18 +27,21 @@ public class InicioMain {
                 maior = novo[op];
             }else if(maior < novo[op]){
                 maior = novo[op];
-            }
-            System.out.println("Esse é o maior valor " + maior);
+            } 
         }
+        System.out.println("Esse é o maior valor " + maior);
 
         for(int conta = 0; conta < novo.length; conta++){
             if(conta == 0){
                 menor = novo[conta];
-            }else if(menor < novo[conta]){
+            }else if(novo[conta] < menor){
+                menor = novo[conta];
                 novo[conta] = menor;
             }
-        
-        System.out.println(novo[conta] + " ");
+        }
+
+        for(int numeros = 0; numeros < novo.length; numeros++){
+            System.out.println(novo[numeros]);
         }
     }
 }
