74ba377e6c6bd9c4a478c8d4cfbe174d51f1e136
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-19 21:44:31 -0300

    construtor

--- Vetores/src/Produtos.java
+++ Vetores/src/Produtos.java
@@ -2,4 +2,13 @@ public class Produtos {
     private String descricao;
     private String embalagem;
     private double valo;
+    
+    public Produtos(String descricao, String embalagem, double valo) {
+        this.descricao = descricao;
+        this.embalagem = embalagem;
+        this.valo = valo;
+    }
+
+
+    
 }
