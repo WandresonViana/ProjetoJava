78c9125f42418290ebbcc4d364d7b15cf19f0bad
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-22 22:08:25 -0300

    Update Produtos.java

--- Vetores/src/Produtos.java
+++ Vetores/src/Produtos.java
@@ -9,6 +9,30 @@ public class Produtos {
         this.valor = valor;
     }
 
+    public String getDescricao() {
+        return descricao;
+    }
 
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getEmbalagem() {
+        return embalagem;
+    }
+
+    public void setEmbalagem(String embalagem) {
+        this.embalagem = embalagem;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    
     
 }
